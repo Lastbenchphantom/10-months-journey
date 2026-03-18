9721726231625511058cb418aa1e9521967ec76b
Zakaria_Revan <zakariazak13913@gmail.com>
2026-03-19 01:01:33 +0600

    new commit

---
+++ 18 march new/index.js
@@ -0,0 +1,36 @@
+// console.log("hello");
+// console.log("I like Raisa");
+
+// document.getElementById("myh1").textContent = "Hello";
+// document.getElementById("par").textContent = "I like Biriyani";
+
+// let age = 25;
+
+// let price = 10.99;
+
+// let gpa = 2.5;
+
+// let firstname = "Zakaria";
+
+// let online = true;
+
+// console.log(typeof age);
+// console.log(`you are ${age} years old`);
+// console.log(`the price is $${price}`);
+// console.log(`your gpa is ${gpa}`);
+
+// console.log(typeof firstname);
+// console.log(`firstname :${firstname}`);
+
+// console.log(typeof online);
+// console.log(`bro is online ${online}`);
+// window.alert("this is an alert");
+
+
+let fullname = "zakaria";
+let age = 25;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullname}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `Your student identity is ${student}`;
